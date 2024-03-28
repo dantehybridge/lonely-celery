38d734fa8fc34213fd6a882c6c8404f2f00d95ae
Dante Samael Fernández Lago <dante2402006@hybridge.education>
2024-03-27 19:08:00 -0500

    Edit 'app.py' file.

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-def app():
-    print("lonely-celery")
+# Here we import the necessary .py files, which hold tuples we need throughout the app.
+import src.confs as confs, src.slugs as slugs, src.tools as tools
 
-if __name__ is "__app__":
+def app():
+    print("Hello, World!")
+
+if __name__ == "__main__":
     app()
